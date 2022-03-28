ee09dd240430d1f1cd9eeff51641355efd661271
Michael Bui <michaelbui99@hotmail.com>
2022-03-28 19:05:07 +0200

    Renamed method in SubredditReposity for consistent naming

--- app/src/main/java/io/github/michaelbui99/atlas/model/repositories/SubredditRepository.kt
+++ app/src/main/java/io/github/michaelbui99/atlas/model/repositories/SubredditRepository.kt
@@ -1,8 +1,10 @@
 package io.github.michaelbui99.atlas.model.repositories
 
+import io.github.michaelbui99.atlas.model.http.responseobjects.SubredditPostDataResponse
+
 interface SubredditRepository {
 
     fun getDefaultSubreddits()
     fun getSubredditPosts(subreddit: String)
-    fun getSubredditThreadData()
+    fun getSubredditPostData(): SubredditPostDataResponse
 }
