8ea3613bdf03fab7c6ab4dc7ff8dd9942643beda
Michael Bui <michaelbui99@hotmail.com>
2022-04-04 18:41:54 +0200

    Setup Tablayout with ViewPager2 in SubredditFragment

--- app/src/main/java/io/github/michaelbui99/atlas/ui/subreddit/SubredditFragment.kt
+++ app/src/main/java/io/github/michaelbui99/atlas/ui/subreddit/SubredditFragment.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import io.github.michaelbui99.atlas.MainActivity
 import io.github.michaelbui99.atlas.R
 
@@ -13,6 +16,7 @@ private const val SUBREDDIT_NAME = "SubredditName"
 class SubredditFragment : Fragment() {
     private var subredditName: String? = null
     private lateinit var viewModel: SubredditViewModel
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +36,11 @@ class SubredditFragment : Fragment() {
     ): View? {
         Log.i("SubredditFragment", "Creating view")
         val rootView = inflater.inflate(R.layout.fragment_subreddit, container, false)
+
+        val vpAdapter = SubredditViewPagerAdapter(this)
+        viewPager = rootView.findViewById(R.id.viewpager_subreddit)
+        viewPager.adapter = vpAdapter
+
         viewModel.subredditPosts.observe(this) {
         }
 
@@ -42,6 +51,16 @@ class SubredditFragment : Fragment() {
         Log.i("SubredditFragment", "Creating menu")
         inflater.inflate(R.menu.subreddit_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val tabTitles = arrayOf("Posts", "About")
+        val tabLayout = view.findViewById<TabLayout>(R.id.tabLayout_subreddit)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onStart() {
---
+++ app/src/main/java/io/github/michaelbui99/atlas/ui/subreddit/SubredditViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package io.github.michaelbui99.atlas.ui.subreddit
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import io.github.michaelbui99.atlas.ui.home.HomeFragment
+
+const val TAB_COUNT = 2
+
+class SubredditViewPagerAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        return TAB_COUNT
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        if (position == 0){
+            return SubredditPostsFragment()
+        }
+
+        // TODO: Change this to SubredditAboutFragment
+        return SubredditAboutFragment()
+    }
+}
